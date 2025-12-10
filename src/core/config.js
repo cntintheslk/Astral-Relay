c79baedd63b92befa4cbd91e7b50f6de85635c2e
Harry Sanderson <cntintheslk@gmail.com>
2025-12-10 04:27:23 +0000

    fix: Added Owner IDs Varaible to Config.js

--- src/core/config.js
+++ src/core/config.js
@@ -5,4 +5,5 @@ module.exports = {
   environment: process.env.NODE_ENV || "production",
   logChannelId: process.env.LOG_CHANNEL_ID,
   devGuildId: process.env.DEV_GUILD_ID,
+  ownerIds: process.env.OWNER_IDS ? process.env.OWNER_IDS.split(",") : [],
 };
