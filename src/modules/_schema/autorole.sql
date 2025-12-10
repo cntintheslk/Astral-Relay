d0362c51d14d2556d2d28400c26a48a0fc547ed3
Harry Sanderson <cntintheslk@gmail.com>
2025-12-10 14:50:46 +0000

    Feat(Web Service): Complete Migration to Web Service to Fascilitate Automatic Changelogs

--- src/modules/_schema/autorole.sql
+++ src/modules/_schema/autorole.sql
@@ -1,17 +1,5 @@
 CREATE TABLE IF NOT EXISTS autorole_settings (
     guild_id TEXT NOT NULL,
     role_id TEXT NOT NULL,
-
     PRIMARY KEY (guild_id, role_id)
 );
-
-
-ALTER TABLE autorole_settings RENAME TO autorole_settings_old;
-
-CREATE TABLE autorole_settings (
-    guild_id TEXT NOT NULL,
-    role_id TEXT NOT NULL,
-    PRIMARY KEY (guild_id, role_id)
-);
-
-DROP TABLE autorole_settings_old;
