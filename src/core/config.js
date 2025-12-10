7fb0a0313e46f9e44b8b93a1b2f46baebd10b2de
Harry Sanderson <cntintheslk@gmail.com>
2025-12-10 03:56:47 +0000

    fix(config): add DEV_GUILD_ID support for slash command deployment

--- src/core/config.js
+++ src/core/config.js
@@ -4,5 +4,5 @@ module.exports = {
   token: process.env.BOT_TOKEN,
   environment: process.env.NODE_ENV || "production",
   logChannelId: process.env.LOG_CHANNEL_ID,
-
+  devGuildId: process.env.DEV_GUILD_ID,
 };
