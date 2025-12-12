31771dcc5c0827ff823039c9446cecb95f8fd22f
Harry Sanderson <cntintheslk@gmail.com>
2025-12-12 11:57:19 +0000

    V2.0.13.5 - Fix(LOA): Bug Fixes - Added Ability to Remove Legacy LOAs from Dev Server

---
+++ src/scripts/clearLegacyLoas.js
@@ -0,0 +1,15 @@
+// scripts/clearLegacyLoas.js
+const db = require("../src/core/database");
+
+const info = db.prepare(`
+  DELETE FROM loa_requests
+  WHERE request_id NOT LIKE '%-%-%-%-%'
+`).run();
+
+const hist = db.prepare(`
+  DELETE FROM loa_history
+  WHERE request_id NOT LIKE '%-%-%-%-%'
+`).run();
+
+console.log("Deleted from loa_requests:", info.changes);
+console.log("Deleted from loa_history:", hist.changes);
